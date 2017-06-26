c13b8db28204bb1ad9378d17e34dce65901800e7
neetesh <neetesh.kumar@qandle.com>
2017-06-26 17:14:53 +0530

    routing basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,33 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import 'hammerjs';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import {DishService} from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import {AppRoutingModule} from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
