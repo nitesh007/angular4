bd7dd15ca86c60fa21a55950e65f1175da91ddf5
neetesh <neetesh.kumar@qandle.com>
2017-08-26 13:47:39 +0530

    weak2 compleat

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {Dish} from '../shared/dish';
+import {DishService} from '../services/dish.service';
+import {Promotion} from '../shared/promotion';
+import {PromotionService} from '../services/promotion.service';
 
 @Component({
   selector: 'app-home',
+  providers: [PromotionService,DishService],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  dish:Dish;
+  promotion:Promotion;
+  constructor(private dishservice : DishService,
+    private promotionservice: PromotionService) { }
 
   ngOnInit() {
+    this.dish = this.dishservice.getFeaturedDish();
+    this.promotion = this.promotionservice.getFeaturedPromotion();
   }
 
 }
